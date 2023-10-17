1f260aef313d186667b540cddd06ddfa45d31009
josephshenq <josephshenq@gmail.com>
2023-10-17 19:25:22 -0400

    LinesPage css file done

--- exer5/marta-interface/src/pages/LinesPage.js
+++ exer5/marta-interface/src/pages/LinesPage.js
@@ -4,16 +4,17 @@ import trainData from '../server/trainData';
 import TrainList from './TrainList.js'
 import MartaButton from '../components/MartaButton.js'
 import NavBar from '../components/NavBar.js'
+import "./LinesPage.css"
 
 export default function LinesPage(props) {
   // initialize some currColor state
 
   return (
     <div>
-        <div></div> // header
-        <div> //body
+        <div></div> 
+        <div className = "body">
             <NavBar color={props.color} data={stationData} />
-            <div>
+            <div className = "trains">
                 <div className="buttons">do
                     <MartaButton name="Arriving"/>
                     <MartaButton name="Scheduled"/>
