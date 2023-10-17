bb98bf3c65f70580746d68c9ee6a205dba7813c7
josephshenq <josephshenq@gmail.com>
2023-10-17 19:16:44 -0400

    Fix Basic Components

--- exer5/marta-interface/src/components/NavBar.js
+++ exer5/marta-interface/src/components/NavBar.js
@@ -3,6 +3,10 @@ import Station from "./Station.js"
 
 export default function NavBar(props) {
     let color = props.color
+    if (color === "GOLD") color = "gold";
+    if (color === "RED") color = "red";
+    if (color === "GREEN") color = "green";
+    if (color === "BLUE") color = "blue";
     let stations = props.data
     return (
         <div> 
