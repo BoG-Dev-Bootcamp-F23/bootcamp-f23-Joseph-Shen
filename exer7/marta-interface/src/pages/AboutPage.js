ce27c41a8c1dd3bcbfc813f438290706cf55b77c
josephshenq <josephshenq@gmail.com>
2023-10-27 15:09:39 -0400

    style about page done

--- exer7/marta-interface/src/pages/AboutPage.js
+++ exer7/marta-interface/src/pages/AboutPage.js
@@ -15,7 +15,7 @@ export default function About() {
             </div>
             <div className="about-content">
                     <img src={map} alt="marta map" className="left" width = {600}></img>
-                    <div className="about-right">
+                    <div className="about-content-right">
                         <h1>Our Mission</h1>
                         <p>The Metropolitan Atlanta Rapid Transit Authority is the principal public transport operator in the Atlanta metropolitan area. Formed in 1971 as strictly a bus system, MARTA operates a network of bus routes linked to a rapid transit system consisting of 48 miles (77 km) of rail track with 38 Subway stations. MARTA's rapid transit system is the eighth-largest rapid transit system in the United States by ridership.</p>
                     </div>
